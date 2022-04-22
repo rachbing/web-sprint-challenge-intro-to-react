e1baac4c7f68d8597eaca7ec90ea000037cb46af
Rachel Bingham <rachelbingham@Rachels-MacBook-Air.local>
2022-04-22 11:36:49 -0600

    character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = (props) => {
+    console.log("character props")
+    console.log(props)
+    return (
+        <div className="character-wrapper">
+            <h3>Name: {props.character.name}</h3>
+        </div>
+    )
+}
+
+export default Character;
